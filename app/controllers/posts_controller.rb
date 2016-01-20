971ccdc7945f86f51e274f390b617fbae3a387e5
goodguymj <s061828@ap.be>
2016-01-20 14:55:00 +0100

    Third commit
    Output in JSON

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,11 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+     @posts = Post.find( params[:id] )
+    respond_to do |format|
+    format.html
+    format.json
+  end
   end
 
   # GET /posts/new
@@ -40,25 +45,25 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+      # if @post.update(post_params)
+        # format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @post }
+      # else
+        # format.html { render :edit }
+        # format.json { render json: @post.errors, status: :unprocessable_entity }
+      # end
+    # end
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    # @post.destroy
+    # respond_to do |format|
+      # format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      # format.json { head :no_content }
+    # end
   end
 
   private
