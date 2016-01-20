35fa06086aa028fbabf4618046c925450a2f6a73
goodguymj <s061828@ap.be>
2016-01-20 15:01:24 +0100

    Fourth Commit
    Changed routes to accept calls from url

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :posts
 
 
+map.create_post 'post/create', :controller => 'posts', :action => 'create', :conditions => { :method => :get }
+
 root :to => "posts#index"
+
 
 end
